78312480b3395f7a1a30294a27e59e0217795c71
mzamarri <132020928+mzamarri@users.noreply.github.com>
2023-05-25 01:14:09 -0700

    Update AllocationForm.js

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -2,7 +2,7 @@ import React, {useContext, useState} from 'react';
 import {AppContext} from '../context/AppContext';
 
 const AllocationForm = (props) => {
-    const {dispatch, remaining} = useContext(AppContext);
+    const {dispatch, remaining, currency} = useContext(AppContext);
 
     const [name, setName] = useState('');
     const [cost, setCost] = useState('');
@@ -58,19 +58,21 @@ const AllocationForm = (props) => {
                         <option defaultvalue value="Add" name="Add">Add</option>
                         <option value="Reduce" name="Reduce">Reduce</option>
                     </select>
-
-                    <input
-                        required='required'
-                        type='number'
-                        id='cost'
-                        value={cost}
-                        style={{marginLeft: '2rem', size: 10}}
-                        onChange={(event) => setCost(event.target.value)}>
-                    </input>
-
-                    <button className="btn btn-primary" onClick={submitEvent} style={{marginLeft: '2rem'}}>
-                        Save
-                    </button>
+                    <div style={{marginLeft: '1rem'}}>
+                        {currency}
+                        <input
+                            required='required'
+                            type='number'
+                            id='cost'
+                            value={cost}
+                            style={{size: 10}}
+                            onChange={(event) => setCost(event.target.value)}>
+                        </input>
+                    </div>
+                    
+                        <button className="btn btn-primary" onClick={submitEvent} style={{marginLeft: '2rem'}}>
+                            Save
+                        </button>
                 </div>
             </div>
         </div>
